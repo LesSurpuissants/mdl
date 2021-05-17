31db5815bb0a944998a5aeacf341811d06d14572
Antoine Lautrette <antoine.lautrette@gmail.com>
2021-05-17 16:58:26 +0200

    Fix oubli d'un use

--- src/Controller/ThemeController.php
+++ src/Controller/ThemeController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
  * @IsGranted("ROLE_USER", message="Vous n'avez pas les droits necessaires pour accéder à cette page")
